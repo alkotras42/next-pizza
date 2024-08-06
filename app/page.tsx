1e8a8932e3bc3bd309e7f1b0786a36435fd49905
alkotras42 <kirill.stogniy@mail.ru>
2024-08-06 15:57:07 +0300

    made categories sticky

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { Categories, Container, SortPopup, Title } from '@/components/shared';
-import { Button } from '@/components/ui/button';
-import Image from "next/image";
+import {Categories, Container, SortPopup, Title, TopBar} from '@/components/shared';
+import {Button} from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <Container className='mt-10'>
-        <Title text="Все пиццы" size='lg' className='font-extrabold'/>
-        <Categories/>
-        <SortPopup/>
+        <Title text='Все пиццы' size='lg' className='font-extrabold' />
       </Container>
+      <TopBar />
+      <div style={{height: '3000px'}}></div>
     </>
   );
 }
