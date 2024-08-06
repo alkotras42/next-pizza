900561d2e7e449c04bf9825b263065f66bc40c0f
alkotras42 <kirill.stogniy@mail.ru>
2024-08-06 15:50:02 +0300

    add categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import { Container, Title } from '@/components/shared';
+import { Categories, Container, SortPopup, Title } from '@/components/shared';
 import { Button } from '@/components/ui/button';
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Container className='mt-5'>
+      <Container className='mt-10'>
         <Title text="Все пиццы" size='lg' className='font-extrabold'/>
+        <Categories/>
+        <SortPopup/>
       </Container>
     </>
   );
